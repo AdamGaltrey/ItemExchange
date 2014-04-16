340ae0a6c729d8ce6fd1b7987cacc388560414a9
Adamki11s <adam.galtrey@gmail.com>
2014-04-16 21:48:30 +0100

    Added Vault economy support

--- src/com/adamki11s/itemexchange/ItemExchange.java
+++ src/com/adamki11s/itemexchange/ItemExchange.java
@@ -3,7 +3,10 @@ package com.adamki11s.itemexchange;
 import java.util.List;
 import java.util.logging.Logger;
 
+import net.milkbowl.vault.economy.Economy;
+
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.adamki11s.itemexchange.database.Database;
@@ -12,12 +15,14 @@ import com.adamki11s.itemexchange.exchange.Exchange;
 import com.adamki11s.itemexchange.exchange.ProfileManager;
 
 public class ItemExchange extends JavaPlugin {
-	
+
 	private static Plugin p;
 	private static Logger l;
-	
+
+	public static Economy economy;
+
 	@Override
-	public void onEnable(){
+	public void onEnable() {
 		p = this;
 		l = getLogger();
 		Config.init();
@@ -25,13 +30,25 @@ public class ItemExchange extends JavaPlugin {
 		List<Entry> entries = Database.loadEntries();
 		Exchange.initExchange(entries);
 		ProfileManager.initProfiles(entries);
+		
+		setupEconomy();
 	}
-	
-	public static Plugin getPlugin(){
+
+	private boolean setupEconomy() {
+		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(
+				net.milkbowl.vault.economy.Economy.class);
+		if (economyProvider != null) {
+			economy = economyProvider.getProvider();
+		}
+
+		return (economy != null);
+	}
+
+	public static Plugin getPlugin() {
 		return p;
 	}
-	
-	public static Logger getLog(){
+
+	public static Logger getLog() {
 		return l;
 	}
 
