c34503f1fa874e492516b6e517e0257b0e210f28
Adamki11s <adam.galtrey@gmail.com>
2014-04-16 20:23:45 +0100

    Asynchronously save new exchange entries

--- src/com/adamki11s/itemexchange/database/Database.java
+++ src/com/adamki11s/itemexchange/database/Database.java
@@ -5,9 +5,15 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
 import com.adamki11s.itemexchange.Config;
 import com.adamki11s.itemexchange.ItemExchange;
 import com.adamki11s.itemexchange.exchange.Entry;
+import com.adamki11s.itemexchange.exchange.Exchange;
 import com.adamki11s.itemexchange.sql.SyncSQL;
 
 public class Database {
@@ -35,7 +41,7 @@ public class Database {
 					 */
 					create = "CREATE TABLE "
 							+ ITEM_TABLE
-							+ "(id INT NOT NULL PRIMARY KEY, seller VARCHAR(36), itemid INTEGER, quantity INTEGER, cpu INTEGER, sold INTEGER, time LONG);";
+							+ "(id INT NOT NULL PRIMARY KEY, seller VARCHAR(36), item VARCHAR(60), quantity INTEGER, cpu INTEGER, sold INTEGER, time LONG);";
 					sql.standardQuery(create);
 					ItemExchange.getLog().info("Item table created.");
 				}
@@ -43,18 +49,20 @@ public class Database {
 				if (!sql.doesTableExist(HISTORY_TABLE)) {
 					create = "CREATE TABLE "
 							+ HISTORY_TABLE
-							+ "(id INT NOT NULL PRIMARY KEY, seller VARCHAR(36), buyer VARCHAR(36), itemid INTEGER, quantity INTEGER, cpu INTEGER, time LONG);";
+							+ "(id INT NOT NULL PRIMARY KEY, seller VARCHAR(36), buyer VARCHAR(36), item VARCHAR(60), quantity INTEGER, cpu INTEGER, time LONG);";
 					sql.standardQuery(create);
 					ItemExchange.getLog().info("History table created.");
 				}
-				
+
 				if (!sql.doesTableExist(DATA_TABLE)) {
-					/*used to keep track of successful trades and record the average price point
-					 * cumulative is the total cost of all sold items, and quantity is used to calculate an average price point
+					/*
+					 * used to keep track of successful trades and record the
+					 * average price point cumulative is the total cost of all
+					 * sold items, and quantity is used to calculate an average
+					 * price point
 					 */
-					create = "CREATE TABLE "
-							+ DATA_TABLE
-							+ "(id INT NOT NULL PRIMARY KEY, itemid INTEGER, cumulative LONG, quantity LONG);";
+					create = "CREATE TABLE " + DATA_TABLE
+							+ "(id INT NOT NULL PRIMARY KEY, item VARCHAR(60), cumulative LONG, quantity LONG);";
 					sql.standardQuery(create);
 					ItemExchange.getLog().info("Data table created.");
 				}
@@ -66,20 +74,42 @@ public class Database {
 			ItemExchange.getLog().severe("SQL connection failed!");
 		}
 	}
-	
-	public static List<Entry> loadEntries(){
+
+	public static List<Entry> loadEntries() {
 		ResultSet s;
 		List<Entry> entries = new ArrayList<Entry>();
 		try {
 			s = sql.sqlQuery("SELECT * FROM " + ITEM_TABLE + ";");
-			while(s.next()){
-				Entry e = new Entry(s.getString("seller"), s.getInt("itemid"), s.getInt("quantity"), s.getInt("cpu"), s.getInt("sold"), s.getLong("time"));
+			while (s.next()) {
+				Entry e = new Entry(s.getString("seller"), Material.valueOf(s.getString("item")), s.getInt("quantity"), s.getInt("cpu"),
+						s.getInt("sold"), s.getLong("time"));
 				entries.add(e);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return entries;
+	}
+
+	public static void addEntryAsync(final Entry e, final Player callback) {
+		Bukkit.getScheduler().runTaskAsynchronously(ItemExchange.getPlugin(), new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					sql.standardQuery("INSERT INTO "
+							+ ITEM_TABLE
+							+ "(seller, item, quantity, cpu, sold, time) VALUES ("
+							+ String.format("%s, %s, %d, %d, %d, %d", e.getSellerUUID(), e.getItem().toString(), e.getListedQuantity(),
+									e.getCostPerUnit(), e.getQuantitySold(), e.getTimeListed()) + ");");
+					Exchange.addEntryAsync(e);
+					callback.sendMessage(ChatColor.GREEN + "Your item was successfully listed on the exchange.");
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+					callback.sendMessage(ChatColor.RED + "There was an error syncing your item with the database.");
+				}
+			}
+		});
 	}
 
 }
