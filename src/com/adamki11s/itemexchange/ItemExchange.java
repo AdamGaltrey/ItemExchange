9b4d756bfac297560d13102ed7b2ed25df48804d
Adamki11s <adam.galtrey@gmail.com>
2014-04-16 18:20:56 +0100

    Initial Commit

---
+++ src/com/adamki11s/itemexchange/ItemExchange.java
@@ -0,0 +1,28 @@
+package com.adamki11s.itemexchange;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ItemExchange extends JavaPlugin {
+	
+	private static Plugin p;
+	private static Logger l;
+	
+	@Override
+	public void onEnable(){
+		p = this;
+		l = getLogger();
+		Files.init();
+	}
+	
+	public static Plugin getPlugin(){
+		return p;
+	}
+	
+	public static Logger getLog(){
+		return l;
+	}
+
+}
